c12be9a1bcc8e71624b3f48c66b717acf0fafc30
jharia <jesikaharia@gmail.com>
2013-02-03 03:14:03 -0500

    transform signal to 0-1

--- sounds/makeTone.m
+++ sounds/makeTone.m
@@ -1,7 +1,12 @@
 function makeTone(Fs,toneFreq,nSeconds,outFile)
     nSeconds = nSeconds/8; 
     y = sin(linspace(0, nSeconds*toneFreq*2*pi, round(nSeconds*Fs)));
+    y = (y+1); 
+    y = y*500/2; 
     final = repmat([y zeros(1,4000)],1,4); 
-    sound(final,Fs); 
+    % sound(final,Fs); 
+    min(final)
+    max(final)
+    length(final)
     wavwrite(final, Fs, 8, outFile);
 end 
