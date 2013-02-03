62abcabfbc507c64f5966a92a7049b79e8ed6c93
Geza Kovacs <gkovacs@mit.edu>
2013-02-03 04:02:20 -0500

    fixed syntax error in parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -11,4 +11,5 @@ function parallaxScroll(){
 	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
 	$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
 	$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
-}
+})
+
