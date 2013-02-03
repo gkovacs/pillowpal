36afde4cdae00ecac01caba61ba58506a3ce2330
Prabhav Jain <prabhav@31-33-95.wireless.csail.mit.edu>
2013-02-03 03:10:42 -0500

    committing image files

---
+++ js/parallax.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	/* Scroll event handler */
+    $(window).bind('scroll',function(e){
+    	parallaxScroll();
+    });
+    
+/* Scroll the background layers */
+function parallaxScroll(){
+	var scrolled = $(window).scrollTop();
+	$('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
+	$('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+	$('#parallax-bg3').css('top',(0-(scrolled*.75))+'px');
+}
